8646dd412b1c01c6e96bf337623432b811cd4c16
Navid Farahzadi <na.farahzadi@gmail.com>
2017-04-14 13:23:31 +0430

    sass added

--- js/dist/vendor.js
+++ js/dist/vendor.js
@@ -63,16 +63,17 @@
 /******/ 	__webpack_require__.p = "js/dist";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
 /******/ })
 /************************************************************************/
-/******/ ([
-/* 0 */,
-/* 1 */
+/******/ ({
+
+/***/ 2:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
 /***/ })
-/******/ ]);
+
+/******/ });
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,4 +1,5 @@
 var path = require('path')
+var ExtractTextPlugin = require('Extract-text-webpack-plugin')
 
 module.exports = {
   target: 'web',
@@ -13,12 +14,24 @@ module.exports = {
   },
   module: {
     rules: [
-      {test: /\.js$/, exclude: /node_modules/, use: 'babel-loader'}
+      {test: /\.js$/, exclude: /node_modules/, use: 'babel-loader'},
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      }
     ]
   },
+  plugins: [
+    new ExtractTextPlugin('../../css/main.css')
+  ],
   watch: true,
   watchOptions: {
     ignored: /node_modules/
   },
+  devtool: 'source-map',
   stats: 'verbose'
 }
